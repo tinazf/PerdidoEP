c221d53bf207874f429833742aa6b973d9251203
Victor-DSR <133363053+Victor-DSR@users.noreply.github.com>
2023-10-15 22:31:59 -0300

    Banco de Dados

---
+++ bdd_pep.sql
@@ -0,0 +1,147 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 16/10/2023 às 03:30
+-- Versão do servidor: 10.4.28-MariaDB
+-- Versão do PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `bdd_pep`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `ficha`
+--
+
+CREATE TABLE `ficha` (
+  `ID` int(11) NOT NULL,
+  `Icone` varchar(255) NOT NULL,
+  `Historico` varchar(255) NOT NULL,
+  `Conceito` varchar(255) NOT NULL,
+  `Porcentagem` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `jogador`
+--
+
+CREATE TABLE `jogador` (
+  `ID` int(11) NOT NULL,
+  `Nome` varchar(255) NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  `Senha` varchar(255) NOT NULL,
+  `Ficha_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `partida`
+--
+
+CREATE TABLE `partida` (
+  `ID` int(11) NOT NULL,
+  `Texto` varchar(255) NOT NULL,
+  `Fase` varchar(255) NOT NULL,
+  `Nivel` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `partida_ficha`
+--
+
+CREATE TABLE `partida_ficha` (
+  `Partida_ID` int(11) NOT NULL,
+  `Ficha_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `ficha`
+--
+ALTER TABLE `ficha`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Índices de tabela `jogador`
+--
+ALTER TABLE `jogador`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `fk_Ficha_Id` (`Ficha_ID`);
+
+--
+-- Índices de tabela `partida`
+--
+ALTER TABLE `partida`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Índices de tabela `partida_ficha`
+--
+ALTER TABLE `partida_ficha`
+  ADD KEY `fk_FichaP_ID` (`Ficha_ID`),
+  ADD KEY `fk_PartidaF_ID` (`Partida_ID`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `ficha`
+--
+ALTER TABLE `ficha`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `jogador`
+--
+ALTER TABLE `jogador`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `ficha`
+--
+ALTER TABLE `ficha`
+  ADD CONSTRAINT `fk_FichaP_ID` FOREIGN KEY (`ID`) REFERENCES `partida_ficha` (`Ficha_ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Restrições para tabelas `jogador`
+--
+ALTER TABLE `jogador`
+  ADD CONSTRAINT `fk_Ficha_Id` FOREIGN KEY (`Ficha_ID`) REFERENCES `ficha` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Restrições para tabelas `partida`
+--
+ALTER TABLE `partida`
+  ADD CONSTRAINT `fk_PartidaF_ID` FOREIGN KEY (`ID`) REFERENCES `partida_ficha` (`Partida_ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
