ec407a0c87737303b967f85cf3e0cd0f65b527ef
Victor-DSR <133363053+Victor-DSR@users.noreply.github.com>
2023-10-22 23:15:45 -0300

    Arquivos Base 1.1

---
+++ TM.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu</title>
+</head>
+<body>
+    <br><a href="TJ.php">Novo Jogo</a>
+    <br>
+    <br><a href="TJS.php">Continuar Jogo</a>
+    <br>
+    <br><a href="TA.php">Diario de Experiencia</a>
+</body>
+</html>
