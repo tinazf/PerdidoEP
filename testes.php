35e26ef830d87fb12dcabd88414a311bdec9f6cf
Victor-DSR <133363053+Victor-DSR@users.noreply.github.com>
2023-11-16 14:35:22 -0200

    Mudanças 4.0

--- testes.php
+++ testes.php
@@ -3,42 +3,15 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
   <link rel="stylesheet" type="text/css" href="auxiliar.css">
-  <style>
-    body {
-      margin: 0;
-      font-family: Arial, sans-serif;
-      background-color: #f4f4f4;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      height: 100vh;
-    }
-
-    #loading-container {
-      text-align: center;
-      display: none;
-    }
-
-    #loading-image {
-      width: 100px; /* Ajuste conforme necessário */
-      height: auto;
-    }
-
-    #content {
-      display: block;
-    }
-  </style>
-  <title>Tela de Carregamento</title>
+  <title>Transição com GIF</title>
 </head>
 <body>
-  <div id="loading-container">
-    <img id="loading-image" src="IMG/Papel.gif" alt="Carregando...">
-  </div>
-    <!-- Seu conteúdo principal aqui -->
-    <div class="container" id="content">
-    <!-- Seu conteúdo aqui -->
-             <form action='codigo.php' class="formulario" method="POST"> 
+  <img class="transition-gif" src="IMG/Papel.gif" alt="GIF de transição">
+  <div class="container">
+  <div class="page current-page" id="pagina1">
+  <form class="formulario" method="POST"> 
         <div class="field">
             <label for="nome">Email:</label>
             <input type="text" name="eml" placeholder='nome@mail.com' required>
@@ -46,20 +19,60 @@
         
         <div class="field">
             <label for="telefone">Senha:</label>
-            <input type="password" name="snh" placeholder='********'>
+            <input type="text" name="snh" placeholder='********'>
         </div>
-        <button class="botao-imagem" type="submit" name="login"> Fazer Anotações </button>
+        <button class="botao-imagem" onclick="navegarParaPagina2()" type="submit" name="login"> Fazer Anotações </button>
+        <button class="botao-imagem" onclick="navegarParaPagina2()"><a style="text-decoration: none;color: inherit;"href="TC.php">Novo Escritor</a></button>
 </form>
-    </div>
+  </div>
+  </div>
+
+  <!-- Página 2 -->
+  <div class="page" id="pagina2">
+    <?php
+    if(isset($_POST['login'])){
+      $email = $_POST['eml'];
+      $senha = $_POST['snh'];
+      if(isset($email) == TRUE){
+              session_start();
+              $_SESSION["idJog"] = $email;
+              $_SESSION["Jogador"] = $senha;
+                  header("location:TM.php");
+          } else {
+                  header("location:index.php");
+          }
+              
+      }
+    ?>
+    <button onclick="navegarParaPagina1()">Voltar para a página 1</button>
   </div>
 
   <script>
-    document.addEventListener("DOMContentLoaded", function() {
-      setTimeout(function() {
-        document.getElementById("loading-container").style.display = "none";
-        document.getElementById("content").style.display = "block";
-      }, 3000); // Tempo de simulação de carregamento (3 segundos no exemplo)
-    });
+    function navegarParaPagina2() {
+      exibirGIF();
+      setTimeout(function () {
+        document.getElementById('pagina1').classList.remove('current-page');
+        document.getElementById('pagina2').classList.add('current-page');
+        ocultarGIF();
+      }, 1000); // Aguarde 1 segundo antes de trocar de página
+    }
+
+    function navegarParaPagina1() {
+      exibirGIF();
+      setTimeout(function () {
+        document.getElementById('pagina2').classList.remove('current-page');
+        document.getElementById('pagina1').classList.add('current-page');
+        ocultarGIF();
+      }, 1000); // Aguarde 1 segundo antes de trocar de página
+    }
+
+    function exibirGIF() {
+      $('.transition-gif').fadeIn();
+    }
+
+    function ocultarGIF() {
+      $('.transition-gif').fadeOut();
+    }
   </script>
 
 </body>
